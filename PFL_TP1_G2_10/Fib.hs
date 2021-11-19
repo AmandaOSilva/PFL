e1fbacb7c3c2ff996f5735624a9bb246f9a54b70
Rafael Camelo <rafael77001@hotmail.com>
2021-11-19 09:44:18 +0000

    bid should be done

--- PFL_TP1_G2_10/Fib.hs
+++ PFL_TP1_G2_10/Fib.hs
@@ -3,10 +3,15 @@ fibRec 0 = 0
 fibRec 1 = 1
 fibRec num = fibRec (num - 1) + fibRec (num - 2)
 
-{--
-fibLista :: (Integral a) => a -> [a]
-fibLista 0 = 0
--}
+
+fibLista :: (Integral a) => a -> a
+auxFibLista :: (Integral a) => a ->[a] -> [a]
+fibLista n = (auxFibLista n []) !! fromIntegral n
+auxFibLista 0 _ = [0]
+auxFibLista 1 _ = [0, 1]
+auxFibLista n fiblis
+  | (length fiblis) <= fromIntegral(n + 1) = auxFibLista (n - 1) fiblis ++ [((auxFibLista (n - 1) fiblis) !! fromIntegral(n - 2)) + ((auxFibLista (n - 1) fiblis) !! fromIntegral(n - 1))]
+  | otherwise = [] --ACABAR
 
 fibListaInfinita :: Int -> Int
 fiblist :: (Integral a) => [a]
