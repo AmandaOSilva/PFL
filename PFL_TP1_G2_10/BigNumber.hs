62e51d3a1844fcad1404f9c2cf2a585ab813c431
Rafael Camelo <rafael77001@hotmail.com>
2021-11-26 19:39:01 +0000

    fixed somaBN

--- PFL_TP1_G2_10/BigNumber.hs
+++ PFL_TP1_G2_10/BigNumber.hs
@@ -31,9 +31,39 @@ output  xs =  concat(map (show) (reverse xs))
 
 
 {-2.4-}
-somaBN:: Bignumber-> Bignumber-> Bignumber
-somaBN x y = somaBN' 0  sameSizexs sameSizeys
-     where (sameSizexs, sameSizeys) = machtSize x y
+isPos :: Bignumber -> Bool
+isPos bn = last bn > 0
+
+toPos :: Bignumber -> Bignumber
+toPos bn = init bn ++ [abs (last bn)]
+
+toNeg :: Bignumber -> Bignumber
+toNeg bn = init bn ++ [abs (last bn) * (-1)]
+
+maiorQue :: Bignumber -> Bignumber -> Bool
+maiorQue x y = bnToInt x > bnToInt y
+
+somaBN :: Bignumber -> Bignumber -> Bignumber
+somaBN x y
+    | isPos x && isPos y =
+          somaBN' 0 sameSizexs sameSizeys
+    | not (isPos x) && not (isPos y) =
+          toNeg (somaBN' 0 sameSizexs sameSizeys)
+    | not (isPos x) && isPos y =
+          if maiorQue posX y then
+                toNeg(subBNaux3 0 (subBNaux2 sameSizexs sameSizeys))
+          else
+                subBNaux3 0 (subBNaux2 sameSizeys sameSizexs)
+    | isPos x && not (isPos y) =
+          if maiorQue x posY then
+                subBNaux3 0 (subBNaux2 sameSizexs sameSizeys)
+          else
+                toNeg(subBNaux3 0 (subBNaux2 sameSizeys sameSizexs))
+    | otherwise = error "ERROR IN somaBN -> isPos"
+          where
+                (sameSizexs, sameSizeys) = machtSize posX posY
+                posX = toPos x
+                posY = toPos y
 
 somaBN':: Int->Bignumber->Bignumber-> Bignumber
 -- somaBN' 0 x []= x
@@ -47,19 +77,19 @@ somaBN' rest (x:xs) (y:ys) = (r `mod` 10 ) : (somaBN' (r `div` 10) xs ys)
     where r = x+y+rest
 
 {-2.5-}
-
 subBN :: Bignumber -> Bignumber ->  Bignumber
-subBN xs ys =  subBNaux3 0 (subBNaux2 xs ys)
+subBN xs ys =  subBNaux3 0 (subBNaux2 sameSizexs sameSizeys)
+     where (sameSizexs, sameSizeys) = machtSize xs ys
 
 subBNaux2 :: Bignumber -> Bignumber -> Bignumber
 subBNaux2 xs ys = zipWith (-) xs ys
 
 subBNaux3 :: Int -> Bignumber -> Bignumber
-subBNaux3 carry (ls:[]) = [ls + carry]
+subBNaux3 carry (ls:[]) = [abs ls - carry]
 subBNaux3 carry (fs:bn)
   | ((fs <= 0 && last bn <= 0) || (fs >= 0 && last bn >= 0)) =
-    if abs fs - carry < 0 then [10 - fs - carry] ++ subBNaux3 1 bn else [abs fs - carry] ++ subBNaux3 0 bn
-  | otherwise = [10 - fs - carry] ++ subBNaux3 1 bn
+    if fs - carry < 0 then [10 + fs - carry] ++ subBNaux3 1 bn else [abs fs - carry] ++ subBNaux3 0 bn
+  | otherwise = [10 + fs - carry] ++ subBNaux3 1 bn
 
 
 
@@ -83,7 +113,7 @@ bnToInt :: Bignumber -> Int
 bnToInt  bn = read (output bn ) :: Int
 
 
-machtSize :: Bignumber -> Bignumber ->  (Bignumber, Bignumber)
+machtSize :: Bignumber -> Bignumber -> (Bignumber, Bignumber)
 machtSize xs ys = (addZeros diff xs , addZeros (-diff) ys)
   where
     diff = length ys - length xs
