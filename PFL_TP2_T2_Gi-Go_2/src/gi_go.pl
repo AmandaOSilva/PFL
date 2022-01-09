2ff8949da6b148ae023813c06af8d6fc590b550e
Amanda Oliveira Silva <43822133+AmandaOSilva@users.noreply.github.com>
2022-01-09 18:29:37 +0000

    calc_score pronto e inicio de testes unitarios

--- PFL_TP2_T2_Gi-Go_2/src/gi_go.pl
+++ PFL_TP2_T2_Gi-Go_2/src/gi_go.pl
@@ -14,7 +14,14 @@ maplistlist(P, [H|T]) :- maplist(P,H), maplistlist(P, T).
 lenght_(A, B) :- length(B, A).
 
 isEqual(A, B) :- A = B.
-
+   
+mirror_gameboard([X|[]], [RevX]) :-
+    rev(X, RevX).    
+mirror_gameboard([X|Xs], Mirror) :-
+    rev(X, RevX),
+    mirror_gameboard(Xs, MirrorXs), !,
+    Mirror = [RevX|MirrorXs].
+    
 replace(Index, OgList, ReplaceTo, NewList) :-
   nth0(Index, OgList, _, Helper),
   nth0(Index, NewList, ReplaceTo, Helper).
@@ -26,20 +33,7 @@ initial_state(Size, GameState) :-
     Turn = 1,
     Score = [0, 0],
     GameState = [GameBoard, Turn, Score].
-/*
-move(GameState, Move, NewGameState) :-
-    nth0(1, GameState, Turn), Turn = 1, 
-    nth0(0, GameState, GameBoard),
-    nth0(2, GameState, Score),
-    doMove(GameBoard, Move, Turn, NewGameBoard),
-    NewGameState = [NewGameBoard, 2, Score];
 
-    nth0(1, GameState, Turn), Turn = 2, 
-        nth0(0, GameState, GameBoard),
-        nth0(2, GameState, Score),
-        doMove(GameBoard, Move, Turn, NewGameBoard),
-        NewGameState = [NewGameBoard, 1, Score].
-*/
 move([GameBoard, Turn, Score], Move, NewGameState) :-
     do_move(GameBoard, Move, Turn, NewGameBoard),
     calc_score(NewGameBoard, Move, Turn, Score, NewScore),
@@ -49,8 +43,9 @@ move([GameBoard, Turn, Score], Move, NewGameState) :-
 calc_score(GameBoard, Move, Turn, Score, NewScore) :-
     calc_score_row(GameBoard, Move, Turn, Score, PartialNewScore1),
     calc_score_column(GameBoard, Move, Turn, PartialNewScore1, PartialNewScore2),
-    calc_score_diagonal_right(GameBoard, Move, Turn, PartialNewScore2, PartialNewScore3),
-    calc_score_diagonal_left(GameBoard, Move, Turn, PartialNewScore3, NewScore).
+    calc_score_diagonal(GameBoard, Move, Turn, PartialNewScore2, PartialNewScore3),
+    mirror_gameboard(GameBoard, MirrorGameBoard),
+    calc_score_diagonal(MirrorGameBoard, Move, Turn, PartialNewScore3, NewScore).
 
 calc_score_row(GameBoard, [MoveX, _], Turn, Score, NewScore) :-
     print('Rol'),
@@ -74,7 +69,7 @@ calc_score_column(GameBoard, [_, MoveY], Turn, Score, NewScore) :-
         nth0(MoveY, Row, Square),
         (
             Square = 'X',
-            print('Col: row= ' + Row),
+            /* ha um quadrado em branco*/
             NewScore = Score;
 
             I =:= L-1,
@@ -82,18 +77,48 @@ calc_score_column(GameBoard, [_, MoveY], Turn, Score, NewScore) :-
             add_score(L, Turn, Score, NewScore)         
         ).
 
-calc_score_diagonal_right(GameBoard, [MoveX,MoveY], Turn, [Score1, Score2], NewScore) :-
+calc_score_diagonal(GameBoard, [MoveX,MoveY], Turn, Score, NewScore) :-
     /* se completar uma diagonal direita */
+    length(GameBoard, L),
+    Diff is MoveY-MoveX,
+    DiagMax is L + abs(Diff),
+    ( 
+        Diff >= 0,
+        repeat,
+            between(0, DiagMax, I),
+            nth0(I, GameBoard, Row),
+            ColNumber is I + Diff,
+            nth0(ColNumber, Row, Square),
+            print(' Diag R: I='+ I + ' colNumber= ' + ColNumber + ' Row' + Row+'\n'),
+            (
+                Square = 'X',
+                print('Col: row= ' + Row),
+                /* ha um quadrado em branco*/
+                NewScore = Score;
 
-    /* se nao completar nada*/
-    NewScore = [Score1, Score2].
+                I =:= DiagMax-1,
+                /* se completar uma coluna */
+                add_score(DiagMax, Turn, Score, NewScore)         
+            );
 
+        Diff < 0,
+        repeat,
+            between(0, DiagMax, I),
+            RowNumber is I - Diff,
+            nth0(RowNumber, GameBoard, Row),
+            nth0(I, Row, Square),
+            print(' Diag R2: I='+ I + ' rowNumber= '+ RowNumber+' Row='+Row),
+            (
+                Square = 'X',
+                print('Col: row= ' + Row),
+                /* ha um quadrado em branco*/
+                NewScore = Score;
 
-calc_score_diagonal_left(GameBoard, [MoveX,MoveY], Turn, [Score1, Score2], NewScore) :-
-    /* se completar uma diagonal esquerda */
-
-    /* se nao completar nada*/
-    NewScore = [Score1, Score2].
+                I =:= DiagMax-1,
+                /* se completar uma coluna */
+                add_score(DiagMax, Turn, Score, NewScore)         
+            )
+    ).
 
 
 add_score(Points, Turn, [Score1, Score2], NewScore ) :-
@@ -121,3 +146,37 @@ do_move(GameBoard, [MoveX,MoveY], Turn, NewGameBoard) :-
     S = [[['X','X','X'],['X','X','X'],['X','X','X']],1,[0,0]]
     move([[['X','X','X'],['X','X','X'],['X','X','X']],1,[0,0]], [1, 1], NewS).
 */
+
+:- use_module(library(plunit)).
+:- begin_tests(test).
+     
+test(mirror_gameboard) :-
+    mirror_gameboard(
+        [['1','2','3'],['4','5','6'],['7','8','9']], 
+        [['3','2','1'],['6','5','4'],['9','8','7']]).
+
+test(initial_state) :-
+    initial_state(3, [[['X','X','X'],['X','X','X'],['X','X','X']], 1, [0,0]]),
+    initial_state(7, [[
+        ['X','X','X','X','X','X','X'],
+        ['X','X','X','X','X','X','X'],
+        ['X','X','X','X','X','X','X'],
+        ['X','X','X','X','X','X','X'],
+        ['X','X','X','X','X','X','X'],
+        ['X','X','X','X','X','X','X'],
+        ['X','X','X','X','X','X','X']], 1, [0,0]]).
+
+test(move_no_score) :- 
+    move(
+        [[['X','X','X'],['X','X','X'],['X','X','X']], 1, [0,0]], [1, 1], 
+        [[['X','X','X'],['X','1','X'],['X','X','X']], 2, [0,0]]), !.
+
+test(move_row_score) :- 
+    move([[['2','X','X'],['1','X','1'],['X','X','X']], 2, [0,0]], [1, 1], 
+         [[['2','X','X'],['1','2','1'],['X','X','X']], 1, [0,3]]), !.
+
+test(move_col_score) :- 
+    move([[['2','X','1'],['X','X','1'],['X','X','X']], 2, [0,0]], [2, 2], 
+         [[['2','X','1'],['X','X','1'],['X','X','2']], 1, [0,3]]), !.
+
+:- end_tests(test).
